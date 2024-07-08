3470cb87acbf294822e0d591b06733ed8ed24a92
pavlidism <mt.pavlidis@gmail.com>
2024-07-08 16:22:24 +0300

    database added

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,4 @@
-require(`dotenv`).config()
+require('dotenv').config()
 
 const express = require("express");
 const app = express();
@@ -20,22 +20,111 @@ app.use(cors({
     origin:"*"
 }))
 
-mongoose.connect("mongodb+srv://Miltiadis:SHA25@cluster0.5konvqu.mongodb.net/chAIrlie")
+mongoose.connect("mongodb+srv://Miltiadis:SHA25@cluster0.5konvqu.mongodb.net/chAIrlie", {
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Error connecting to MongoDB', err);
+});
 
-const User = mongoose.model(`User`, {})
 
-app.post('/chat', async (req, res) => {
-    try {
-      const { messages } = req.body;
-      const chatCompletion = await openai.chat.completions.create({
-        messages,
-        model: 'gpt-3.5-turbo',
-      });
-      res.json(chatCompletion);
-    } catch (error) {
-      res.status(500).send('Error communicating with OpenAI API');
-    }
-  });
+const simpleSchema = new mongoose.Schema({
+  date: {type: Date, default: Date.now},
+  content:
+    [{userText: String,
+      PAtext: String,
+    }]
+});
+
+const SimpleModel = mongoose.model('SimpleModel', simpleSchema);
+
+app.post('/chat/new', async (req, res) => {
+  try {
+      const newDocument = await SimpleModel.create(req.body); // Create a new document directly
+      res.status(201).json(newDocument);
+  } catch (error) {
+      res.status(500).json({ error: 'Failed to create document' });
+  }
+});
+
+app.delete('/chat/:id', async (req, res) => {
+  try {
+      const DBRes = await SimpleModel.deleteOne({ _id: req.params.id });
+      if (DBRes.deletedCount === 0) {
+          res.status(404).json({ error: 'Document not found' });
+      } else {
+          res.json(DBRes);
+      }
+  } catch (error) {
+      res.status(500).json({ error: 'Failed to delete document' });
+  }
+});
+
+app.get('/chat', async (req, res) => {
+  try {
+      const interactions = await SimpleModel.find();
+      res.json(interactions);
+  } catch (error) {
+      res.status(500).json({ error: 'Failed to retrieve documents' });
+  }
+});
+
+app.get('/chat/:id', async (req, res) => {
+  try {
+      const interactions = await SimpleModel.find({ _id: req.params.id });
+      res.json(interactions);
+  } catch (error) {
+      res.status(500).json({ error: 'Failed to retrieve documents' });
+  }
+});
+
+app.post('/chat/:id', async (req, res) => {
+  try {
+    const {id} = req.params;
+    const { messages } = req.body;
+    console.log(`Received chat ID: ${id}`)
+    console.log(`Messages1: ${JSON.stringify(messages)}`);
+    console.log(`${messages[0].content}`)
+    const chatCompletion = await openai.chat.completions.create({
+      messages,
+      model: 'gpt-3.5-turbo',
+    });
+    const userMessage = `${messages[0].content}`;
+    const assistantMessage = `${chatCompletion.choices[0].message.content}`;
+    const updatedDocument = await SimpleModel.findByIdAndUpdate(
+      id,
+      { $push: { content: { userText: userMessage, PAtext: assistantMessage } } },
+      { new: true }
+    );
+    res.json(chatCompletion);
+    console.log(`${chatCompletion.choices[0].message.role}`)
+    console.log(`${chatCompletion.choices[0].message.content}`)
+  } catch (error) {
+    res.status(500).send('Error communicating with OpenAI API');
+  }
+});
+
+// app.post('/chat/:id', async (req, res) => {
+//     try {
+//       const {id} = req.params;
+//       const { messages } = req.body;
+//       console.log(`Received chat ID: ${id}`)
+//       console.log(`Messages2: (messages)}`);
+      
+//       const chatCompletion = await openai.chat.completions.create({
+//         messages,
+//         model: 'gpt-3.5-turbo',
+//       });
+      
+//       console.log(messages[0].content)
+//       res.json(chatCompletion);
+//       console.log(chatCompletion.choices[0].message.role)
+//       console.log(chatCompletion.choices[0].message.content)
+
+//     } catch (error) {
+//       res.status(500).send('Error communicating with OpenAI API');
+//     }
+//   });
 
 app.get(`/`, (req,res)=>{
     res.send(`Hello world`)
--- Frontend/chairlie/src/components/ChatPage.jsx
+++ Frontend/chairlie/src/components/ChatPage.jsx
@@ -1,14 +1,44 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './ChatPage.css';
 
 function ChatPage() {
     const [userInput, setUserInput] = useState("");
     const [userMessages, setUserMessages] = useState([]);
     const [assistantMessages, setAssistantMessages] = useState([]);
+    const [chatId, setChatId] = useState(null);
+    const [inputEnabled, setInputEnabled] = useState(false);
+    const [listOfChats, setListOfChats] = useState([]);
+
+    useEffect(() => {
+        getAllChats();
+    }, []);
+
+    function getAllChats() {
+        axios.get("http://localhost:3002/chat").then(({ data }) => {
+            console.log(data);
+            setListOfChats(data);
+        });
+    }
 
     function handleUserInput(e) {
         setUserInput(e.target.value);
+    }
+
+    async function createNewChat() {
+        try {
+            const response = await axios.post('http://localhost:3002/chat/new');
+            const newChatId = response.data._id;
+            setChatId(newChatId);
+            setUserMessages([]);
+            setAssistantMessages([]);
+            setInputEnabled(true);
+            console.log(response.data)
+            console.log(newChatId)
+            getAllChats()
+        } catch (error) {
+            console.error('Error creating new chat:', error);
+        }
     }
 
     async function sendMessage() {
@@ -19,7 +49,7 @@ function ChatPage() {
         setUserInput("");
 
         try {
-            const response = await axios.post('http://localhost:3002/chat', {
+            const response = await axios.post(`http://localhost:3002/chat/${chatId}`, {
                 messages: [{ role: 'user', content: userMessage }],
             });
             console.log(response.data);
@@ -27,8 +57,36 @@ function ChatPage() {
             setAssistantMessages(prev => [...prev, assistantMessage]);
         } catch (error) {
             console.error('Error:', error);
+            const assistantMessage = "Oops, it appears there was an error"
+            setAssistantMessages(prev => [...prev, assistantMessage])
         }
     }
+
+    function deleteChat(historyId) {
+        axios.delete(`http://localhost:3002/chat/${historyId}`).then(response => {
+            console.log(`Poll deleted:${historyId}`, response.data);
+            getAllChats();
+        }).catch(error => {
+            console.error(`There was an error deleting the poll`, error);
+        });
+    }
+    // async function sendMessage() {
+    //     const userMessage = userInput.trim();
+    //     if (!userMessage || !chatId) return;
+
+    //     setUserMessages(prev => [...prev, userMessage]);
+    //     setUserInput("");
+
+    //     try {
+    //         const response = await axios.post(`http://localhost:3002/chat/${chatId}`, {
+    //             message: { role: 'user', content: userMessage }, - no square brackets
+    //         });
+    //         const assistantMessage = response.data.choices[0].message.content;
+    //         setAssistantMessages(prev => [...prev, assistantMessage]);
+    //     } catch (error) {
+    //         console.error('Error sending message:', error);
+    //     }
+    // }
 
     function handleKeyDown(e) {
         if (e.key === 'Enter') {
@@ -38,15 +96,29 @@ function ChatPage() {
 
     return (
         <div className="container">
-            <div className="chatDisplay">
-                {userMessages.map((message, index) => (
-                    <div key={index} className="message">
-                        <p className="userMessage"> {message}</p>
-                        {assistantMessages[index] && (
-                            <p className="assistantMessage">{assistantMessages[index]}</p>
-                        )}
+            <div className="interactivePanel">
+                <div className="historyDisplay">
+                    <div>
+                        <button onClick={createNewChat}>New chat</button>
+                        {listOfChats.map((e,i)=>(
+                            <div >
+                                {e.date}
+                                <button onClick={()=>deleteChat(e._id)}>X</button>
+                            </div>
+                        ))}
                     </div>
-                ))}
+                    
+                </div>
+                <div className="chatDisplay">
+                    {userMessages.map((message, index) => (
+                        <div key={index} className="message">
+                            <p className="userMessage">{message}</p>
+                            {assistantMessages[index] && (
+                                <p className="assistantMessage">{assistantMessages[index]}</p>
+                            )}
+                        </div>
+                    ))}
+                </div>
             </div>
             <div className="chatInput">
                 <input
@@ -54,8 +126,9 @@ function ChatPage() {
                     value={userInput}
                     onChange={handleUserInput}
                     onKeyDown={handleKeyDown}
+                    disabled={!inputEnabled}
                 />
-                <button onClick={sendMessage}>Send</button>
+                <button onClick={sendMessage} disabled={!inputEnabled}>Send</button>
             </div>
         </div>
     );
